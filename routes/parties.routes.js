0386a0145210d05da008efaf2c4988eed6d0951b
Kwassi05 <k.akagankou@ecole-ipssi.net>
2026-02-11 14:22:09 +0100

    parties.routes.js

--- routes/parties.routes.js
+++ routes/parties.routes.js
@@ -1,7 +1,18 @@
-// Routes des parties (placeholder)
 const express = require('express');
 const router = express.Router();
+const { 
+  createPartie, 
+  getPartie, 
+  avancer, 
+  attaquer 
+} = require('../controllers/parties.controller');
 
-router.get('/', (req, res) => res.json({ message: 'Liste des parties (placeholder)' }));
+
+const requireAuth = require('../utils/auth.utils'); 
+
+router.post('/', requireAuth, createPartie);
+router.get('/:id', requireAuth, getPartie);
+router.post('/:id/avancer', requireAuth, avancer);
+router.post('/:id/attaquer', requireAuth, attaquer);
 
 module.exports = router;
